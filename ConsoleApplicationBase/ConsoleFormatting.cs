78dcc8442a34a7346654d1b2751eb651bf53a296
xivsolutions <xivsolutions@gmail.com>
2014-09-07 08:10:01 -0500

    Add Formatting module and indentation method

---
+++ ConsoleApplicationBase/ConsoleFormatting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationBase
+{
+    public class ConsoleFormatting
+    {
+        public static string Indent(int count)
+        {
+            return "".PadLeft(count);
+        }
+    }
+}
